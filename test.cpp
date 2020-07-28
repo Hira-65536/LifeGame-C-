19bf062a4a3162eee8160ea09ebfc11d60f14ba7
hira65536 <retera5198myon@gmail.com>
2020-07-28 14:16:50 +0900

    上下左右末端をループさせるようにした。

--- test.cpp
+++ test.cpp
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <conio.h>
 
-#define MAP_WIDTH 20
-#define MAP_HEIGHT 20
+#define WIDTH 10
+#define HEIGHT 10
+#define MAP_WIDTH WIDTH+2
+#define MAP_HEIGHT HEIGHT+2
 
 //printf cells
 void print_cells(int (&a)[MAP_HEIGHT][MAP_WIDTH],int cnt);
@@ -13,11 +15,14 @@ int lives_counter(int (&a)[MAP_HEIGHT][MAP_WIDTH],int _x, int _y);
 //compare now step => next step
 void step_cells(int (&before)[MAP_HEIGHT][MAP_WIDTH], int (&after)[MAP_HEIGHT][MAP_WIDTH]);
 
+
 int main()
 {
     int cnt=1;
+
     int cell_a[MAP_HEIGHT][MAP_WIDTH]={{}};
     int cell_b[MAP_HEIGHT][MAP_WIDTH]={{}};
+
 
     int (*a)[MAP_HEIGHT][MAP_WIDTH] = &cell_a;
     int (*b)[MAP_HEIGHT][MAP_WIDTH] = &cell_b;
@@ -37,17 +42,40 @@ int main()
 
 void print_cells(int (&a)[MAP_HEIGHT][MAP_WIDTH],int cnt)
 {
-    int x=0, y=0;
+    int x=1, y=1;
     int break_flag=0;
     //int cells[MAP_HEIGHT][MAP_WIDTH]={{}};
     while(true)
     {
+        if(x<=0)x=1;
+        if(x>=MAP_WIDTH-1) x=MAP_WIDTH-2;
+        if(y<=0)y=1;
+        if(y>=MAP_HEIGHT-1) y=MAP_HEIGHT-2;
         system("cls");
         printf("CURSOR MOVE:wasd SET:SPACE NEXT:r \n");
         for (int i = 0; i < MAP_HEIGHT; i++)
         {
             for (int j = 0; j < MAP_WIDTH; j++)
             {
+                if(i==0){
+                    a[0][j]=a[MAP_HEIGHT-2][j];
+                    printf("%s",a[i][j]?"œ":"Z");
+                    continue;
+                }else if(i==MAP_HEIGHT-1){
+                    a[MAP_HEIGHT-1][j]=a[1][j];
+                    printf("%s",a[i][j]?"œ":"Z");
+                    continue;
+                }
+                if(j==0){
+                    a[i][0]=a[i][MAP_WIDTH-2];
+                    printf("%s",a[i][j]?"œ":"Z");
+                    continue;
+                }else if(j==MAP_WIDTH-1){
+                    a[i][MAP_WIDTH-1]=a[i][1];
+                    printf("%s",a[i][j]?"œ":"Z");
+                    continue;
+                }
+
                 if ((x == j) && (y == i))
                     printf("™");
                 else
@@ -56,6 +84,7 @@ void print_cells(int (&a)[MAP_HEIGHT][MAP_WIDTH],int cnt)
                     printf("@");
                 else
                     printf("%d",lives_counter(a,j,i));*/
+                
             }
             printf("\n");
         }
@@ -77,11 +106,15 @@ void print_cells(int (&a)[MAP_HEIGHT][MAP_WIDTH],int cnt)
 
 int lives_counter(int (&a)[MAP_HEIGHT][MAP_WIDTH],int _x, int _y){
     int count=0;
+
     for(int y=-1;y<=1;y++){
         for(int x=-1;x<=1;x++){
             if((x==0)&&(y==0)) continue;
-            int loop_WIDTH = (MAP_WIDTH + _x + x) % MAP_WIDTH;
-            int loop_HEIGHT = (MAP_HEIGHT + _y + y) % MAP_HEIGHT;
+
+            int loop_WIDTH = _x + x;
+            int loop_HEIGHT = _y + y;
+            //int loop_WIDTH = (MAP_WIDTH + _x + x) % MAP_WIDTH;
+            //int loop_HEIGHT = (MAP_HEIGHT + _y + y) % MAP_HEIGHT;
             count += a[loop_HEIGHT][loop_WIDTH];
         }
     }
@@ -90,10 +123,12 @@ int lives_counter(int (&a)[MAP_HEIGHT][MAP_WIDTH],int _x, int _y){
 
 void step_cells(int (&before)[MAP_HEIGHT][MAP_WIDTH], int (&after)[MAP_HEIGHT][MAP_WIDTH])
 {
-    for (int y = 0; y < MAP_HEIGHT; y++)
+
+    for (int y = 1; y < MAP_HEIGHT-1; y++)
         {
-            for (int x = 0; x < MAP_WIDTH; x++)
+            for (int x = 1; x < MAP_WIDTH-1; x++)
             {
+            
             int n = lives_counter(before,x,y);
             int dead_or_alive = before[y][x];
             if(before[y][x]){
